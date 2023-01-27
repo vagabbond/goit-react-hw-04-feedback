647cb1a1c3abd25e82fb52b6f092a3752a20cccb
Maks Thimbarov <imaksim2@icloud.com>
2023-01-27 18:55:07 +0200

    done

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 
 import ButtonList from './ButtonsList';
 import Statistics from './Statistics';
@@ -7,57 +7,56 @@ import Notification from './Notification';
 
 import { FeedbackContainer } from './Feedback.styled';
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  hadleClick = e => {
+  const state = { good, neutral, bad };
+
+  const hadleClick = e => {
     const { textContent } = e.target;
-    this.setState(prevState => {
-      return {
-        [textContent.toLowerCase()]: prevState[textContent.toLowerCase()] + 1,
-      };
-    });
+    console.log(textContent);
+    switch (textContent) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+    }
   };
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
+  const countPositiveFeedbackPercentage = () => {
     return Math.round((good * 100) / (good + neutral + bad));
   };
-  render() {
-    return (
-      <FeedbackContainer>
-        <Section title="Please leave feedback">
-          <ButtonList
-            options={Object.keys(this.state)}
-            hadleClick={this.hadleClick}
-          ></ButtonList>
+  return (
+    <FeedbackContainer>
+      <Section title="Please leave feedback">
+        <ButtonList
+          options={Object.keys(state)}
+          hadleClick={hadleClick}
+        ></ButtonList>
+      </Section>
+      {countTotalFeedback() === 0 ? (
+        <Notification message="There is no feedback"></Notification>
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            options={Object.keys(state)}
+            feedbackArr={[good, neutral, bad]}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          ></Statistics>
         </Section>
-        {this.countTotalFeedback() === 0 ? (
-          <Notification message="There is no feedback"></Notification>
-        ) : (
-          <Section title="Statistics">
-            <Statistics
-              options={Object.keys(this.state)}
-              feedbackArr={[
-                this.state.good,
-                this.state.neutral,
-                this.state.bad,
-              ]}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            ></Statistics>
-          </Section>
-        )}
-      </FeedbackContainer>
-    );
-  }
-}
+      )}
+    </FeedbackContainer>
+  );
+};
 
 export default Feedback;
